748834bacca55b5bf4fda042ec887019c1bf2059
Andrew Snead <andrewcsnead@gmail.com>
2022-05-20 18:50:19 +1000

    initial commit

---
+++ serverless.ts
@@ -0,0 +1,43 @@
+import type { AWS } from '@serverless/typescript';
+
+import runner from '@functions/runner';
+
+const serverlessConfiguration: AWS = {
+  service: 'marclip-code-runner',
+  frameworkVersion: '3',
+  useDotenv: true,
+  plugins: ['serverless-esbuild', 'serverless-offline'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    apiGateway: {
+      apiKeys: [{ value: '${env:RUNNER_SERVICE_API_KEY}' }],
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    },
+  },
+  // import the function via paths
+  functions: { runner },
+  package: { individually: true },
+  custom: {
+    'serverless-offline': {
+      httpPort: 4005,
+    },
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ['aws-sdk'],
+      target: 'node16',
+      define: { 'require.resolve': undefined },
+      platform: 'node',
+      concurrency: 10,
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
